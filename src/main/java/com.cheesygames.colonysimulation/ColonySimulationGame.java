50ee29fcedc40a1195e1cd3f2830c8bc1a27b98c
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-07-03 10:26:23 -0400

    Added build.gradle. Added .gitignore. Added basic ColonySimulationGame class.

---
+++ src/main/java/com.cheesygames.colonysimulation/ColonySimulationGame.java
@@ -0,0 +1,13 @@
+package com.cheesygames.colonysimulation;
+
+public class ColonySimulationGame extends Game {
+
+    public static void main(String[] args) {
+        new ColonySimulationGame().start();
+    }
+
+    @Override
+    protected void updateGame() {
+
+    }
+}
