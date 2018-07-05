2de27a989ed144b4f3d6f44b59152824147f16eb
Benoît Dubreuil <b.dubreuil@hotmail.com>
2018-07-05 17:30:00 -0400

    Added the visuals for the chunk

--- src/main/java/com.cheesygames.colonysimulation/ColonySimulationGame.java
+++ src/main/java/com.cheesygames.colonysimulation/ColonySimulationGame.java
@@ -1,13 +1,98 @@
 package com.cheesygames.colonysimulation;
 
+import com.cheesygames.colonysimulation.asset.DefaultMaterial;
+import com.cheesygames.colonysimulation.assets.Asset;
+import com.cheesygames.colonysimulation.math.vector.Vector3i;
+import com.cheesygames.colonysimulation.world.World;
+import com.cheesygames.colonysimulation.world.chunk.Chunk;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.material.RenderState;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.debug.Arrow;
+
 public class ColonySimulationGame extends Game {
+
+    private World m_world;
+    private Material m_material;
 
     public static void main(String[] args) {
         new ColonySimulationGame().start();
     }
 
     @Override
+    public void simpleInitApp() {
+        super.simpleInitApp();
+
+        m_world = new World();
+        m_world.generateWorld();
+        m_world.generateMeshes();
+
+        initLights();
+        addChunk();
+
+        flyCam.setMoveSpeed(flyCam.getMoveSpeed() * 25f);
+        attachCoordinateAxes(Vector3f.ZERO.clone());
+    }
+
+    @Override
     protected void updateGame() {
 
+    }
+
+    private void attachCoordinateAxes(Vector3f pos) {
+        Arrow arrow = new Arrow(Vector3f.UNIT_X);
+        putShape(arrow, ColorRGBA.Red).setLocalTranslation(pos);
+
+        arrow = new Arrow(Vector3f.UNIT_Y);
+        putShape(arrow, ColorRGBA.Green).setLocalTranslation(pos);
+
+        arrow = new Arrow(Vector3f.UNIT_Z);
+        putShape(arrow, ColorRGBA.Blue).setLocalTranslation(pos);
+    }
+
+    private Geometry putShape(Mesh shape, ColorRGBA color) {
+        Geometry g = new Geometry("coordinate axis", shape);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.getAdditionalRenderState().setWireframe(true);
+        mat.getAdditionalRenderState().setLineWidth(4);
+        mat.setColor("Color", color);
+        g.setMaterial(mat);
+        g.setLocalScale(10f);
+        rootNode.attachChild(g);
+        return g;
+    }
+
+    protected void addChunk() {
+        Chunk chunk = m_world.getChunkAt(Vector3i.ZERO);
+        Geometry chunkGeom = new Geometry("", chunk.getMesh());
+        chunkGeom.setLocalTranslation(chunk.getSize().x / -2f, chunk.getSize().y / -2f, chunk.getSize().z / -2f);
+
+        m_material = new Material(assetManager, DefaultMaterial.LIGHTING.getPath());
+        m_material.setBoolean("UseMaterialColors",true);
+        m_material.setColor("Ambient",ColorRGBA.White);
+        m_material.setColor("Diffuse",ColorRGBA.White);
+        m_material.setColor("Specular",ColorRGBA.White);
+        m_material.setFloat("Shininess", 64f);  // [0,128]
+        m_material.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Off);
+        m_material.getAdditionalRenderState().setWireframe(true);
+
+        chunkGeom.setMaterial(m_material);
+        rootNode.attachChild(chunkGeom);
+    }
+
+    protected void initLights() {
+        DirectionalLight directionalLight = new DirectionalLight();
+        directionalLight.setDirection(Vector3f.UNIT_XYZ.mult(-1).normalizeLocal());
+        directionalLight.setColor(ColorRGBA.White);
+        rootNode.addLight(directionalLight);
+
+        AmbientLight ambientLight = new com.jme3.light.AmbientLight();
+        ambientLight.setColor(ColorRGBA.White.mult(0.1f));
+        rootNode.addLight(ambientLight);
     }
 }
